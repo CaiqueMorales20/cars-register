968acf0ab7d1854b366579684e1ab00768950aa6
Caique Morales <caiquemorales20@gmail.com>
2024-10-17 15:55:54 -0300

    chore: add shadcn-ui + prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <main>Ready to code</main>
+  return (
+    <main className="flex h-full w-full">
+      <Button>Send</Button>
+    </main>
+  )
 }
